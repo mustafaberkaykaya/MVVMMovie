56d4e4d7eabb0ca21ab08f9f81f134dbffa5ef33
Berkay <berkaykaya_2000@hotmail.comgit config --global user.name mustafaberkaykaya>
2022-11-12 16:02:10 +0300

    delete sceneDeleegate and add App Routing

---
+++ MVVMMovie/App/AppRouter.swift
@@ -0,0 +1,21 @@
+//
+//  AppRouter.swift
+//  MVVMMovie
+//
+//  Created by Berkay on 12.11.2022.
+//
+
+import UIKit
+
+final class AppRouter {
+
+    var window: UIWindow?
+
+    init() {}
+    // swiftlint:disable all
+    func startApplication() {
+        guard let window = window else { fatalError("Window value cannot be nil") }
+        let viewController = ViewController()
+        window.rootViewController = viewController
+    }
+}
