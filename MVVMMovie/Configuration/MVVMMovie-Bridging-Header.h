22a2b7be428458c04437025ae8cda610d25141e8
Berkay <berkaykaya_2000@hotmail.comgit config --global user.name mustafaberkaykaya>
2022-11-13 02:43:37 +0300

    add BridgingHeader

---
+++ MVVMMovie/Configuration/MVVMMovie-Bridging-Header.h
@@ -0,0 +1,12 @@
+//
+//  MVVMMovie-Bridging-Header.h
+//  MVVMMovie
+//
+//  Created by Berkay on 13.11.2022.
+//
+
+
+#import <DataProvider/DataProvider.h>
+#import <UIComponents/UIComponents.h>
+
+
