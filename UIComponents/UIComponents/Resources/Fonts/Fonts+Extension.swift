f4689c9a5d02862b6d0c7b299f19d8ed02eecbac
Berkay <berkaykaya_2000@hotmail.comgit config --global user.name mustafaberkaykaya>
2022-11-12 22:43:33 +0300

    add fonts

---
+++ UIComponents/UIComponents/Resources/Fonts/Fonts+Extension.swift
@@ -0,0 +1,40 @@
+//
+//  Fonts+Extension.swift
+//  UIComponents
+//
+//  Created by Berkay on 12.11.2022.
+//
+
+import Foundation
+import UIKit
+
+public extension UIFont {
+    
+    static let interMedium24 = FontFamily.Inter.medium.font(size: 24)
+    static let interRegular24 = FontFamily.Inter.regular.font(size: 24)
+    static let interBold24 = FontFamily.Inter.bold.font(size: 24)
+    
+    static let interMedium20 = FontFamily.Inter.medium.font(size: 20)
+    static let interRegular20 = FontFamily.Inter.regular.font(size: 20)
+    static let interBold20 = FontFamily.Inter.bold.font(size: 20)
+    
+    static let interMedium18 = FontFamily.Inter.medium.font(size: 18)
+    static let interRegular18 = FontFamily.Inter.regular.font(size: 18)
+    static let interBold18 = FontFamily.Inter.bold.font(size: 18)
+    
+    static let interMedium16 = FontFamily.Inter.medium.font(size: 16)
+    static let interRegular16 = FontFamily.Inter.regular.font(size: 16)
+    static let interBold16 = FontFamily.Inter.bold.font(size: 16)
+    
+    static let interMedium14 = FontFamily.Inter.medium.font(size: 14)
+    static let interRegular14 = FontFamily.Inter.regular.font(size: 14)
+    static let interBold14 = FontFamily.Inter.bold.font(size: 14)
+    
+    static let interMedium12 = FontFamily.Inter.medium.font(size: 12)
+    static let interRegular12 = FontFamily.Inter.regular.font(size: 12)
+    static let interBold12 = FontFamily.Inter.bold.font(size: 12)
+    
+    static let interMedium10 = FontFamily.Inter.medium.font(size: 10)
+    static let interRegular10 = FontFamily.Inter.regular.font(size: 10)
+    static let interBold10 = FontFamily.Inter.bold.font(size: 10)
+}
