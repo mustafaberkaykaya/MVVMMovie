06182f63b098256197ab0a418c76e0674521b271
Berkay <berkaykaya_2000@hotmail.comgit config --global user.name mustafaberkaykaya>
2022-11-12 22:43:57 +0300

    add Assets

---
+++ UIComponents/UIComponents/Resources/Assets/UIColor+Colors.swift
@@ -0,0 +1,66 @@
+// swiftlint:disable all
+// Generated using SwiftGen — https://github.com/SwiftGen/SwiftGen
+
+import UIKit
+
+// swiftlint:disable identifier_name line_length nesting type_body_length type_name
+public extension UIColor {
+
+  static var appBackgroundGray: UIColor {
+      return Asset.Colors.appBackgroundGray.color
+  }
+
+  static var appBlue: UIColor {
+      return Asset.Colors.appBlue.color
+  }
+
+  static var appCherry: UIColor {
+      return Asset.Colors.appCherry.color
+  }
+
+  static var appCloud: UIColor {
+      return Asset.Colors.appCloud.color
+  }
+
+  static var appHoney: UIColor {
+      return Asset.Colors.appHoney.color
+  }
+
+  static var appLight: UIColor {
+      return Asset.Colors.appLight.color
+  }
+
+  static var appObsidian: UIColor {
+      return Asset.Colors.appObsidian.color
+  }
+
+  static var appPear: UIColor {
+      return Asset.Colors.appPear.color
+  }
+
+  static var appPearl: UIColor {
+      return Asset.Colors.appPearl.color
+  }
+
+  static var appPopupBg: UIColor {
+      return Asset.Colors.appPopupBg.color
+  }
+
+  static var appSmoke: UIColor {
+      return Asset.Colors.appSmoke.color
+  }
+
+  static var appSnow: UIColor {
+      return Asset.Colors.appSnow.color
+  }
+
+  static var appSuggestionRecipeBackground: UIColor {
+      return Asset.Colors.appSuggestionRecipeBackground.color
+  }
+
+  static var appWhite: UIColor {
+      return Asset.Colors.appWhite.color
+  }
+}
+// swiftlint:enable identifier_name line_length nesting type_body_length type_name
+
