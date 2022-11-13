231e2d350fcbf7f7f775b72f95e187e62a9e089b
Berkay <berkaykaya_2000@hotmail.comgit config --global user.name mustafaberkaykaya>
2022-11-13 16:11:23 +0300

    add configure UI Splash Screen

--- MVVMMovie/Scenes/Splash/SplashViewController.swift
+++ MVVMMovie/Scenes/Splash/SplashViewController.swift
@@ -6,11 +6,59 @@
 //
 
 import UIKit
+import TinyConstraints
 
 final class SplashViewController: BaseViewController<SplashViewModel> {
+    
+    private lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.font = .interBold18
+        label.textColor = .appBlue
+        return label
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        addSubViews()
+        subscribeViewModel()
+        viewModel.viewDidLoad()
     }
 }
 
+// MARK: - UI Layout
+extension SplashViewController {
+    
+    private func addSubViews() {
+        addTitleLabel()
+    }
+    
+    private func addTitleLabel() {
+        view.addSubview(titleLabel)
+        titleLabel.centerInSuperview()
+    }
+}
+
+// MARK: - SubscribeViewModel
+extension SplashViewController {
+    
+    private func subscribeViewModel() {
+        viewModel.updateTitle = { [weak self] text in
+            guard let self = self else { return }
+            self.titleLabel.text = text
+        }
+    }
+}
+
+// MARK: - RoutingDelegate
+extension SplashViewController: SplashViewRouteDelegate {
+    
+    func goToHomePage() {
+        guard let window = app.router.window else { return }
+        let homeViewController = HomeRouter.createHomeScreen()
+        UIView.transition(with: window, duration: 1, options: .transitionFlipFromBottom, animations: {
+            UIView.performWithoutAnimation {
+                window.rootViewController = homeViewController
+            }
+        }, completion: nil)
+    }
+}
