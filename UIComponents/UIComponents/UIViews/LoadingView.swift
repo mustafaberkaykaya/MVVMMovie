fed58c88c3dcc8af5408aa73ffc7ebc446b3ff79
Berkay <berkaykaya_2000@hotmail.comgit config --global user.name mustafaberkaykaya>
2022-11-13 16:04:01 +0300

    add animationView

---
+++ UIComponents/UIComponents/UIViews/LoadingView.swift
@@ -0,0 +1,59 @@
+//
+//  LoadingView.swift
+//  UIComponents
+//
+//  Created by Berkay on 13.11.2022.
+//
+
+import Foundation
+import UIKit
+import Lottie
+import TinyConstraints
+
+public class LoadingView: UIView {
+    
+    private var animationView = AnimationView(name: "spinner")
+    
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        addSubViews()
+        configureContents()
+    }
+    
+    // swiftlint:disable fatal_error unavailable_function
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+// MARK: - UI Layout
+extension LoadingView {
+    
+    private func addSubViews() {
+        addAnimationView()
+    }
+    
+    private func addAnimationView() {
+        addSubview(animationView)
+        animationView.centerInSuperview()
+    }
+}
+
+// MARK: - Configure
+extension LoadingView {
+    
+    private func configureContents() {
+        backgroundColor = .appPopupBg.withAlphaComponent(0.25)
+        configureAnimationView()
+    }
+    
+    private func configureAnimationView() {
+        animationView.contentMode = .scaleAspectFit
+        animationView.size(.init(width: 150, height: 150))
+        animationView.loopMode = .loop
+        animationView.animationSpeed = 0.75
+        animationView.backgroundBehavior = .pauseAndRestore
+        animationView.play()
+    }
+}
+
