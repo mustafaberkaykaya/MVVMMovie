527be37125c5d274e0c789df7d7a2e196ad0de39
Berkay <berkaykaya_2000@hotmail.comgit config --global user.name mustafaberkaykaya>
2022-11-15 01:05:53 +0300

    add routing for detail Screen

--- MVVMMovie/Scenes/Home/HomeRouter.swift
+++ MVVMMovie/Scenes/Home/HomeRouter.swift
@@ -5,12 +5,16 @@
 //  Created by Berkay on 13.11.2022.
 //
 
+import UIKit
+
 final class HomeRouter {
 
-    static func createHomeScreen() -> HomeViewController {
+    static func createHomeScreen() -> UINavigationController {
         let service = Service()
         let viewModel = HomeViewModel(service: service)
         let viewController = HomeViewController(viewModel: viewModel)
-        return viewController
+        viewModel.routeDelegate = viewController
+        let navigationController = UINavigationController(rootViewController: viewController)
+        return navigationController
     }
 }
