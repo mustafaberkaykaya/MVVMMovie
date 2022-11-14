24e279201410f1a114b88fb405c4b52423e3d930
Berkay <berkaykaya_2000@hotmail.comgit config --global user.name mustafaberkaykaya>
2022-11-14 16:04:33 +0300

    update WebService

--- DataProvider/DataProvider/Service/WebService.swift
+++ DataProvider/DataProvider/Service/WebService.swift
@@ -8,13 +8,14 @@
 import Alamofire
 
 public protocol ServiceProtocol {
-     func searchMovie(movieName: String, page: Int, onSuccess: @escaping (Movies?) -> (), onError: @escaping(AFError) -> ())
+     func searchMovie(movieName: String, page: Int, onSuccess: @escaping (Movies?) -> (), onError: @escaping(AFError?) -> ())
 }
 
 public final class Service: ServiceProtocol {
     public init() { }
-    public func searchMovie(movieName: String, page: Int, onSuccess: @escaping (Movies?) -> (), onError: @escaping (AFError) -> ()) {
-        ServiceManager.shared.fetch(path: Constant.ServiceEndPoint.searchMovie(searchMovieName: movieName, page: page)) { (response: Movies) in
+    
+    public func searchMovie(movieName: String, page: Int, onSuccess: @escaping (Movies?) -> (), onError: @escaping (AFError?) -> ()) {
+        ServiceManager.shared.fetch(path: Constant.ServiceEndPoint.searchMovie(searchMovieName: movieName, page: page).addingPercentEncoding(withAllowedCharacters: .urlFragmentAllowed) ?? "") { (response: (Movies)) in
             onSuccess(response)
         } onError: { error in
             onError(error)
